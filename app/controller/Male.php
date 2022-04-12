0e54cd6fa30f112d3a36021f0538fb65baa591fb
zhangxfq <zhangxfq@yonyou.com>
2022-04-12 18:54:00 +0800

    修改页面跳转

--- app/controller/Male.php
+++ app/controller/Male.php
@@ -4,18 +4,17 @@ namespace app\controller;
 use Error;
 use Exception;
 use app\model\Counters;
+use think\db\Fetch;
 use think\response\Html;
 use think\response\Json;
 use think\facade\Log;
+use think\facade\View;
+use think\view\driver\Think;
+
 class Male
 {
-    /**
-     * 主页静态页面
-     * @return Html
-     */
-    public function index(): Html
+    public function index()
     {
-        # html路径: ../view/index.html
-        return response(file_get_contents(dirname(dirname(__FILE__)).'/view/male/index.html'));
+        return view::fetch('male/index',['num'=>rand(100,1000)]);
     }
 }
